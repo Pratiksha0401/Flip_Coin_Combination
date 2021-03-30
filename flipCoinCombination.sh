da6868bc9987de15eee190f72d75c7ffce432d07
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-03-30 16:11:56 +0530

    Added: code to store singlet combinations in a dictionary

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,10 +1,22 @@
 #!/bin/bash -x
+declare -A singlet
 H=1
 T=0
-randomNum=$((RANDOM%2))
-if [ $randomNum -eq $H ]
-then
-	echo "Head won"
-else
-	echo "Tail won"
-fi
+countH=0
+countT=0
+read -p "Enter a number for which you wnat to flip coin:" n
+for (( i=1;i<=n;i++ ))
+do
+	randomNum=$((RANDOM%2))
+	if [ $randomNum -eq $H ]
+	then
+		singlet[$i]="H"
+		((countH++))
+	else
+		singlet[$i]="T"
+		((countT++))
+	fi
+done
+echo "Singlet Combinations are ${singlet[@]}"
+echo "Percentage of H is $(((($countH*100))/n))"
+echo "Percentage of T is $(((($countT*100))/n))"
